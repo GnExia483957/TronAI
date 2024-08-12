553ee5c40def75f400cc6f48e71adbf75ec0899d
NgaiGarrick <garrick.ngai@tron.network>
2024-08-12 18:08:25 +0800

    fixed double click bug in AIQ&A button

--- js/scripts.js
+++ js/scripts.js
@@ -68,10 +68,10 @@ function getAPI() {
 
   function toggleAnimation() {
     let x = document.getElementById("animation-container");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
   }
   ////////////////////////////////////////////////////////////////////////////////////
