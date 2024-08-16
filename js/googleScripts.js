4be126d8b91aeb1238d6248a31bf65469cb99bf1
NgaiGarrick <garrick.ngai@tron.network>
2024-08-16 11:50:46 +0800

    test script for google widget

---
+++ js/googleScripts.js
@@ -0,0 +1,12 @@
+if (!window["WIZ_gab_global_data"]) {
+    window["WIZ_gab_global_data"] = {
+    'zkAi6d': 'https:\/\/cloud.google.com\/ai\/gen-app-builder\/_\/UcsWidgetUi',
+    'axVgKc': 'https:\/\/www.gstatic.com\/_\/mss\/boq-cloud-ai-ucs\/_\/js\/k\x3dboq-cloud-ai-ucs.UcsWidgetUi.en_US.7034hoXvBVE.2021.O\/am\x3drGA\/d\x3d1\/rs\x3dAAj4NmyVTer7vtkibxOuC7qsSfOHXPm8YQ\/m\x3dbootstrap_module,ucs_widget_module',
+    'CD1P0b': "CtACEg8I\/PLhFRgAOgZSVXl2UGQSDwim8+AVGAE6BnI2RW44YxIPCLfR4BUYAToGQzFYdm1iEg8I7+jhFRgBOgZDeTd0NGISDwj+nOIVGAA6BkVlNFFXZBIPCOjx4BUYAToGeXpHSG9mEg8IvYviFRgAOgZZc1NEWmISDwiQkeIVGAA6Bm9xUmtyYxIPCKKE4hUYADoGRkJkUlhiEg8I3cjhFRgBOgZraDJtVGMSDwjD9OEVGAA6Bmx4SkdZYxIPCPuS4hUYADoGb1IyZXFmEg8IxpThFRgBOgZHMmhNc2USDwjA3OEVGAE6Bk1BS1MyZRIOCIne4RUYAToFRVlwdFkSDwjl8eEVGAA6BmJ3a0NnYxIPCJOc4hUYADoGZm1VRFZlEg8Iuf3hFRgAOgZpalJQdGMaHQgDEhkdEOX7uCkWz7AF1wuqhATT2QX5uQa6FMhs",
+}; 
+    const s = document.createElement("script");
+    s.src = "https:\/\/www.gstatic.com\/_\/mss\/boq-cloud-ai-ucs\/_\/js\/k\x3dboq-cloud-ai-ucs.UcsWidgetUi.en_US.7034hoXvBVE.2021.O\/am\x3drGA\/d\x3d1\/rs\x3dAAj4NmyVTer7vtkibxOuC7qsSfOHXPm8YQ\/m\x3dbootstrap_module,ucs_widget_module";
+    s.async = true;
+    s.crossorigin = "anonymous";
+    document.head.appendChild(s);
+} 
