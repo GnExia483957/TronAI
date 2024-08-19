00c24e86c9a0416d6187023904b6b1314f4e46f9
NgaiGarrick <garrick.ngai@tron.network>
2024-08-19 16:11:30 +0800

    re-enable widget

--- js/AIScripts.js
+++ js/AIScripts.js
@@ -1,7 +1,6 @@
 const apiUrl = 'https://discoveryengine.googleapis.com/v1alpha/projects/1008121697399/locations/global/collections/default_collection/engines/tron-q-a_1723642895525/servingConfigs/default_search:search';
 const authToken = 'Bearer ya29.c.c0ASRK0GbL-krZ_TSKH2qPKjmIKbtSa3Rnd9EytJmCHblZ75ZX42vyfjfoqDxTcP0yjafHQaCKaHAXYTWlEO162QCRmoLhHOp_sHgiPGglvK5Yg8vQ62EJc3rPvCUtcB34i2ac8Qr25R8LF9bSMvPp4Y50jGfNf90_dpLxFR9VTkA0fAj_s7PTmo65sT4qqRCDfc1sw3VexlUiy7_5ciyY449dwIO5Ms3ZCRWJtykB66l4UtSKWMriD_S1zrITHyyWNLjMHSz7R96JbxRMj8o7Qhz4lWHE7xm-WjQAr0nnggnMQWCck-FXdMJF0FG23DkAe7XW53OZPZ0tWFsIftRbtzR5ZjwEP9NpASeIUx-GYUiZ2sT7wkk4YN_UNO91Kwaz8zkbegYU5-QTEMKky3tWgZXnftZ39oCiAbAlcfwVI82kZvMF8enZYrIuS1zfS_IxQQKipOXSL4rh0uOTKxgWq0mtUitV3ucG-eNTKShTKmBPSFPmTZgb-mTIcV_Izjek12-zeyD_5_jfyWk5ACHSS4tmPdTOpaHtCW_zeVGgofh1ftN6_RLFg3Gu99_B_1MlXLBX9ETrhlMgg2cQbh1FzNFc7sjqjWaXPrZT6tzyUG_RX8zeioEFV3g76NDa4QT647KsbZufJcbqtXrgR9RbtQX-MSXq_fqxxtBe47mxmfm2gmZx_3Orj7ZhMeRI023qyBYs0rfva868lnZFSXeVl5RlV894gevJvSYiBlw6rhoBtQ2dtWempoao4nZ2tw_pVqbh5J1B0uy349Zi88psfknI8rJRZhrJVzIaikhs6z7_Sw5SvmSw_oSclJWdnfZYx4_u3fM5Ifl3necv-3ZzJyeUXBWxdukM6eVhbXxfqJWVOpoIncfOcgfqVeXvW2pXdBF6nhg_qU6vY5g1dWzM1Fv5I6yeadw5esQjMhmwqUnj22vvpM33uxg3X46rdib40UUWnIzfzVrvOst3cpigpkpo7g5SlWg-5gX4sQ4tM7iYv5jxu5VaYds';
-let query = 'what is tron';
-const pageSize = 3;
+const pageSize = 10;
 const session = 'projects/1008121697399/locations/global/collections/default_collection/engines/tron-q-a_1723642895525/sessions/-';
 const spellCorrectionSpec = { mode: 'AUTO' };
 
@@ -38,7 +37,7 @@ function getValue() {
     // Display the value after 5 seconds
     // 5 秒后显示输入值
     setTimeout(function() {
-      displayInput(value);
+      aiSearch(value);
     }, 5000);
   } else {
     setTimeout(function() {
@@ -47,15 +46,15 @@ function getValue() {
   }
 }
 
-function displayInput(value) {
-  // Append the input value to the output element
-  // 将输入值添加到输出元素中
-  $('.output').append(`<div>${value}</div>`);
+// function displayInput(value) {
+//   // Append the input value to the output element
+//   // 将输入值添加到输出元素中
+//   $('.output').append(`<div>${value}</div>`);
   
-  // Toggle the visibility of the loading container
-  // 切换加载容器的可见性
-  loadingContainer.classList.toggle('visible');
-}
+//   // Toggle the visibility of the loading container
+//   // 切换加载容器的可见性
+//   loadingContainer.classList.toggle('visible');
+// }
 
 function errorInput(){
   console.log("There is no input");
@@ -63,7 +62,7 @@ function errorInput(){
 }
 
 
-// Make the AJAX request
+//Make the AJAX request
 // function apiCall1(query){
 // fetch(apiUrl, {
 //   method: 'POST',
@@ -88,10 +87,11 @@ function errorInput(){
 //   // Handle any errors
 //   console.error(error);
 // });
-// }
+// //}
 
 ////////////////////////////////////////////////////////////////////////////////////
 
+function aiSearch(query){
 
 // Set the request parameters
 const apiUrl2 = 'https://discoveryengine.googleapis.com/v1alpha/projects/1008121697399/locations/global/collections/default_collection/engines/tron-q-a_1723642895525/servingConfigs/default_search:answer';
@@ -111,8 +111,6 @@ const answerGenerationSpec = {
 };
 
 // Make the AJAX request
-function aiSearch(query2){
-
   fetch(apiUrl2, {
     method: 'POST',
     headers: {
@@ -137,12 +135,11 @@ function aiSearch(query2){
     console.error(error);
   });
 
-}
-
-function appendChat(data){
-  $('.output').append(`
-    <div class="AI-Container>
-      <div id="ai-text">${data.answer.answerText}</div>
-    </div>
-  `);
+  function appendChat(data){
+    $('.output').append(`
+      <div class="AI-Container>
+        <div id="ai-text">${data.answer.answerText}</div>
+      </div>
+    `);
+  }
 }
